b589bd4051cf00e7fca7f3d50b918c2f01873bb3
rodgerchency <yschen0005@gmail.com>
2020-10-26 22:50:32 +0800

    [MOD] 優化處理速度

--- hw2/load.py
+++ hw2/load.py
@@ -13,11 +13,11 @@ objects = ijson.items(f, 'item')
 
 path = 'data.txt'
 data={}
-with open(path) as jsonFile:
+with open(path, encoding="utf-8") as jsonFile:
     data = json.load(jsonFile)
     
 cnt =0;
-for article in list(objects):
+for article in list(objects)[100000:300000]:
   cnt = cnt+1
   print(cnt)
   content = article["content"].split("/")  
@@ -26,16 +26,29 @@ for article in list(objects):
           continue;
       temp = word.split()
       tword = temp[0]
+      if len(temp) > 1:
+          ttype = temp[1]
+          if ttype == 'x' or ttype == 'v' or ttype == 'ud'\
+          or ttype == 'ug' or ttype == 'uj' or ttype == 'ul'\
+          or ttype == 'uv' or ttype == 'uz'\
+          or ttype == 'm' or ttype == 'mg' or ttype == 'mq'\
+          or ttype == 'a' or ttype == 'p' or ttype == 'c':
+              continue
+      else:
+          continue;          
+      if len(tword) == 1:
+          continue
       if tword in data:
           data[tword]["cnt"] = int(data[tword]["cnt"]) + 1
           if article['id'] not in data[tword]['id']:
               data[tword]['id'].append(article['id']);              
       else:
-           data[tword] = {};
-           data[tword] = {
-               'cnt': 1,
-               'id' : [article['id']]
-               };
+            data[tword] = {};
+            data[tword] = {
+                'cnt': 1,
+                'type':ttype,
+                'id' : [article['id']]
+                };
           
   # name = article['title']
   # data[name]=[]
